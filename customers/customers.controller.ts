90513347d7610abb7ca5854a0e2e7f39faa7e852
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 12:48:12 +0400

    Update customers.controller.ts

--- customers/customers.controller.ts
+++ customers/customers.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
-import {CustomersService} from './customers.service';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { CustomersService } from './customers.service';
 
 @Controller('customers')
 export class CustomersController {
-  constructor(private readonly service: CustomersService) {}
+  constructor(private readonly customersService: CustomersService) {}
+
+  // Make sure this exists! If it is missing, you will get "Cannot POST"
+  @Post()
+  create(@Body() createCustomerDto: any) {
+    return this.customersService.create(createCustomerDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.customersService.findAll();
+  }
 }
