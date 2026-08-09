ad19797c54be26a2e7d47bf6a95aa8a02913b0b4
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 23:04:31 +0400

    Update reports.module.ts

--- reports/reports.module.ts
+++ reports/reports.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import {ReportsService} from './reports.service';
-import {ReportsController} from './reports.controller';
+import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
 
 @Module({
-  providers: [ReportsService],
   controllers: [ReportsController],
-  exports: [ReportsService],
+  providers: [ReportsService],
 })
 export class ReportsModule {}
