aa5a2dad5d0f5ee66d084097a668e8dc194451d4
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 15:47:14 +0400

    Update invoices.module.ts

--- invoices/invoices.module.ts
+++ invoices/invoices.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InvoicesController } from './invoices.controller';
 import { InvoicesService } from './invoices.service';
+import { InvoicesExtraService } from './invoices-extra.service'; // <-- Import it
 
 @Module({
   controllers: [InvoicesController],
-  providers: [InvoicesService],
+  providers: [InvoicesService, InvoicesExtraService], // <-- Add it here
 })
 export class InvoicesModule {}
