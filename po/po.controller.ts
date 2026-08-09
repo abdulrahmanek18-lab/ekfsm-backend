a441f0a57b57af341155a4685136dba82261e584
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 17:25:36 +0400

    Update po.controller.ts

--- po/po.controller.ts
+++ po/po.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
-import {POService} from './po.service';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { PoService } from './po.service';
 
 @Controller('po')
-export class POController {
-  constructor(private readonly service: POService) {}
+export class PoController {
+  constructor(private readonly poService: PoService) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.poService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.poService.findAll();
+  }
 }
