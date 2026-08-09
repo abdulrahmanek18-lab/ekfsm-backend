afc5b459be788cd3099c3377390917c1b7192cd6
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 15:05:03 +0400

    Update amc.controller.ts

--- amc/amc.controller.ts
+++ amc/amc.controller.ts
@@ -1,22 +1,17 @@
-import { Controller, Get, Post, Patch, Delete, Body, Param, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AmcService } from './amc.service';
 
 @Controller('amc')
 export class AmcController {
-  constructor(private readonly service: AmcService) {}
+  constructor(private readonly amcService: AmcService) {}
 
   @Post()
-  create(@Body() dto: any) { return this.service.create(dto); }
+  create(@Body() body: any) {
+    return this.amcService.create(body);
+  }
 
   @Get()
-  findAll(@Query('companyId') companyId: string) { return this.service.findAll(companyId); }
-
-  @Get(':id')
-  findOne(@Param('id') id: string, @Query('companyId') companyId: string) { return this.service.findOne(id, companyId); }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: any) { return this.service.update(id, dto.companyId, dto); }
-
-  @Delete(':id')
-  remove(@Param('id') id: string, @Query('companyId') companyId: string) { return this.service.remove(id, companyId); }
+  findAll() {
+    return this.amcService.findAll();
+  }
 }
