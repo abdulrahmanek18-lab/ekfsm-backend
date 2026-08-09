8b1ff38d8524d0bbb64a150c46330151347d8ab8
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 23:05:07 +0400

    Update reports.controller.ts

--- reports/reports.controller.ts
+++ reports/reports.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
-import {ReportsService} from './reports.service';
+import { Controller, Get } from '@nestjs/common';
+import { ReportsService } from './reports.service';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly service: ReportsService) {}
+
+  @Get('summary')
+  getSummary() {
+    return this.service.getSummary();
+  }
 }
