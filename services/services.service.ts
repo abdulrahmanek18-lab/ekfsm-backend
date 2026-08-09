a75d5ee8eefeaf91fcca8d701704c876002a7ffe
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 14:52:53 +0400

    Update services.service.ts

--- services/services.service.ts
+++ services/services.service.ts
@@ -4,4 +4,19 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class ServicesService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: any) {
+    return this.prisma.serviceCategory.create({
+      data: {
+        name: data.name,
+        description: data.description || null,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.serviceCategory.findMany({
+      orderBy: { createdAt: 'desc' },
+    });
+  }
 }
