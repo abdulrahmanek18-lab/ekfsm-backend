5e86203b7b061627a7e6370f65bd2fff7822b5f5
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-09 13:42:13 +0400

    Update customers.controller.ts

--- customers/customers.controller.ts
+++ customers/customers.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 
 @Controller('customers')
