6757e27d233cd125db3e3826fcb8bb36f4e09009
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-11 22:11:38 +0400

    Update company.controller.ts

--- company/company.controller.ts
+++ company/company.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
-import {CompanyService} from './company.service';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CompanyService } from './company.service';
 
 @Controller('company')
 export class CompanyController {
   constructor(private readonly service: CompanyService) {}
+
+  @Get()
+  getSettings() {
+    return this.service.getSettings();
+  }
+
+  @Post()
+  updateSettings(@Body() body: any) {
+    return this.service.updateSettings(body);
+  }
 }
