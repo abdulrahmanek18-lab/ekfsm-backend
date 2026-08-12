28f4028fedb4fbd5cd5da6c785dbb8579afe9106
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-13 01:19:39 +0400

    Update amc.controller.ts

--- amc/amc.controller.ts
+++ amc/amc.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AmcService } from './amc.service';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('amc')
+@Roles('SUPER_ADMIN', 'ADMIN', 'MANAGER', 'ACCOUNTANT')
 export class AmcController {
-  constructor(private readonly amcService: AmcService) {}
+  constructor(private readonly service: AmcService) {}
 
   @Post()
   create(@Body() body: any) {
-    return this.amcService.create(body);
+    return this.service.create(body);
   }
 
   @Get()
   findAll() {
-    return this.amcService.findAll();
+    return this.service.findAll();
   }
 }
