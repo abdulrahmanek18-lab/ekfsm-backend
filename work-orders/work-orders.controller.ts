d2b3d11fbc225963af4f9efde223b0e3fcc2b7e9
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-13 00:55:13 +0400

    Update work-orders.controller.ts

--- work-orders/work-orders.controller.ts
+++ work-orders/work-orders.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Patch, Body, Param } from '@nestjs/common';
 import { WorkOrdersService } from './work-orders.service';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('work-orders')
+@Roles('SUPER_ADMIN', 'ADMIN', 'MANAGER', 'COORDINATOR', 'TECHNICIAN') // <--- ADD THIS
 export class WorkOrdersController {
   constructor(private readonly service: WorkOrdersService) {}
 
