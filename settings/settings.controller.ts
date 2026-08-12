678eab66cb63cfeff1d8093f68edc2849db666ff
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-13 00:58:52 +0400

    Update settings.controller.ts

--- settings/settings.controller.ts
+++ settings/settings.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { SettingsService } from './settings.service';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('settings')
+@Roles('SUPER_ADMIN', 'ADMIN') // <--- ADD THIS: Only Admins can change settings
 export class SettingsController {
   constructor(private readonly service: SettingsService) {}
 
