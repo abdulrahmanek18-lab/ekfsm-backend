190bedbcf588655386d455c91303f0538e2dad85
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-13 01:20:38 +0400

    Update reports.controller.ts

--- reports/reports.controller.ts
+++ reports/reports.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('reports')
+@Roles('SUPER_ADMIN', 'ADMIN', 'MANAGER', 'ACCOUNTANT')
 export class ReportsController {
   constructor(private readonly service: ReportsService) {}
 
