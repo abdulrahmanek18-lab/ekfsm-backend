1859eb1aa3957144a0dcd49017905b8784d0998b
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-13 00:51:37 +0400

    Update expenses.controller.ts

--- expenses/expenses.controller.ts
+++ expenses/expenses.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ExpensesService } from './expenses.service';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('expenses')
+@Roles('SUPER_ADMIN', 'ADMIN', 'MANAGER', 'ACCOUNTANT') // <--- ADD THIS
 export class ExpensesController {
   constructor(private readonly expensesService: ExpensesService) {}
 
