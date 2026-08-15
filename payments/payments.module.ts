c08236ab8b6b2f46b965dfb45583b1074149ebd7
abdulrahmanek18-lab <abdulrahmanek18@gmail.com>
2026-08-15 11:55:33 +0400

    Update payments.module.ts

--- payments/payments.module.ts
+++ payments/payments.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PaymentsController } from './payments.controller';
-import { PaymentsService } from './payments.service';
+import { ReceiptsController } from './receipts.controller';
+import { ReceiptsService } from './receipts.service';
 
 @Module({
-  controllers: [PaymentsController],
-  providers: [PaymentsService],
+  controllers: [ReceiptsController], // <--- IF THIS IS MISSING, IT WON'T WORK
+  providers: [ReceiptsService],
 })
-export class PaymentsModule {}
+export class ReceiptsModule {}
